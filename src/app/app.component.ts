afbdfba4bce4095cde1f0eddd686ea7cc7d3e8d2
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-08-19 10:49:18 +0200

    feat: Add profile component and update app component to include profile

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavigationComponent } from './navigation/navigation.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavigationComponent],
+  imports: [
+    RouterOutlet,
+    NavigationComponent,
+    ProfileComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
