049404fe583ad53e144a6c4c4986a9be16f52133
Aleandro Presta <37838171+AleandroPresta@users.noreply.github.com>
2024-11-02 16:09:39 +0100

    Deleted skills component

--- src/app/skills/Skill.ts
+++
@@ -1,9 +0,0 @@
-// A Skill class with two attributes, name and experience level
-export class Skill {
-    name: string;
-    experience: string;
-    constructor(name: string, experience: string) {
-        this.name = name;
-        this.experience = experience;
-    }
-}
